97f57d8f35a314cbb7c939b060de583ccf2d8c3e
VLKARLO <145991636+VLKARLO@users.noreply.github.com>
2023-09-30 21:24:29 +0800

    Add files via upload

---
+++ LOG1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+</head>
+<body>
+
+<h2>Modal Login Form</h2>
+
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+
+
+</body>
+</html>
